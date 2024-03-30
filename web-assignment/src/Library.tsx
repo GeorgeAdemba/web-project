474f95743613d762c3f623167125aa13f2802972
GeorgeAdemba <georgeade.47@gmail.com>
2024-03-30 19:58:31 +0300

    added library

--- web-assignment/src/Library.tsx
+++ web-assignment/src/Library.tsx
@@ -1,98 +1,62 @@
-import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
-import "./Card.css";
-import Row from "react-bootstrap/Row";
+import Component from "./Library[FUNC]";
+import "./App.css";
+import { Row } from "react-bootstrap";
 
-function Library(){
-    return(
-        <>
-        <h4>Library</h4>
-      <Row xs={4} md={3} className="g-4">
-        <div className="card-display">
-          <Card className="cards">
-            <Card.Img
-              variant="top"
-              src="https://m.media-amazon.com/images/I/810t2nP7GDL._SL1500_.jpg"
+function Library() {
+  return (
+    <>
+      <div>App</div>
+      <Row xs={2} md={3} className="g-4">
+        <div className="recommendations">
+          <div className="recommendations">
+            <Component
+              bookImage="https://m.media-amazon.com/images/I/810t2nP7GDL._SL1500_.jpg"
+              title="Jurassic world"
+              description="about dinosaurs"
+              author="some guy"
+              genre="sci-fi"
             />
-            <Card.Body>
-              <Card.Title>God Speaks My Language</Card.Title>
-              <Card.Text>
-                A History of Bible Translation in the land of East Africa.
-              </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroup.Item>Title: God Speaks My Language</ListGroup.Item>
-              <ListGroup.Item>Author: Aloo Osoti Mojola</ListGroup.Item>
-              <ListGroup.Item>Genre: History</ListGroup.Item>
-            </ListGroup>
-            <Card.Body>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-          </Card>
-        </div>
-
-        <div className="card-display">
-          <Card className="cards">
-            <Card.Img
-              variant="top"
-              src="https://m.media-amazon.com/images/I/712ITBOaNbL._SL1360_.jpg"
+          </div>
+          <div className="recommendations">
+            <Component
+              bookImage="https://m.media-amazon.com/images/I/810t2nP7GDL._SL1500_.jpg"
+              title="Jurassic world"
+              description="about dinosaurs"
+              author="some guy"
+              genre="sci-fi"
             />
-            <Card.Body>
-              <Card.Title>
-                East African Language and Culture: A Review Study
-              </Card.Title>
-              <Card.Text>
-                A study of the various languages and culture of East Africa
-              </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroup.Item>
-                Title: East African Language and Culture: A Review Study
-              </ListGroup.Item>
-              <ListGroup.Item>Author: Snehangshu Shekhar Chanda</ListGroup.Item>
-              <ListGroup.Item>Genre: Study</ListGroup.Item>
-            </ListGroup>
-            <Card.Body>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-          </Card>
-        </div>
-
-        <div className="card-display">
-          <Card className="cards">
-            <Card.Img
-              variant="top"
-              src="https://m.media-amazon.com/images/I/61+JUDlVVLL._SL1000_.jpg"
+          </div>
+          <div className="recommendations">
+            <Component
+              bookImage="https://m.media-amazon.com/images/I/810t2nP7GDL._SL1500_.jpg"
+              title="Jurassic world"
+              description="about dinosaurs"
+              author="some guy"
+              genre="sci-fi"
             />
-            <Card.Body>
-              <Card.Title>
-                Ethnicity, Identity, and Conceptualizing Community in Indian
-                Ocean East Africa
-              </Card.Title>
-              <Card.Text>
-                An Indian Ocean Studies Series of communities along the coast of
-                East Africa
-              </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroup.Item>
-                Title: Ethnicity, Identity, and Conceptualizing Community in
-                Indian Ocean East Africa
-              </ListGroup.Item>
-              <ListGroup.Item>Author: Daren E. Ray</ListGroup.Item>
-              <ListGroup.Item>Genre: Study</ListGroup.Item>
-            </ListGroup>
-            <Card.Body>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-          </Card>
+          </div>
+          <div>
+            <Component
+              bookImage="https://m.media-amazon.com/images/I/810t2nP7GDL._SL1500_.jpg"
+              title="Jurassic world"
+              description="about dinosaurs"
+              author="some guy"
+              genre="sci-fi"
+            />
+          </div>
+          <div className="recommendations">
+            <Component
+              bookImage="https://m.media-amazon.com/images/I/810t2nP7GDL._SL1500_.jpg"
+              title="Jurassic world"
+              description="about dinosaurs"
+              author="some guy"
+              genre="sci-fi"
+            />
+          </div>
         </div>
       </Row>
     </>
-    );
+  );
 }
 
-export default Library
+export default Library;
---
+++ web-assignment/src/Library[FUNC].tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import "./Card.css";
+import Row from "react-bootstrap/Row";
+
+function Component(props: {
+  bookImage: string | undefined;
+  title:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | Iterable<React.ReactNode>
+    | null
+    | undefined;
+  description:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | Iterable<React.ReactNode>
+    | React.ReactPortal
+    | null
+    | undefined;
+  author:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | Iterable<React.ReactNode>
+    | React.ReactPortal
+    | null
+    | undefined;
+  genre:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | Iterable<React.ReactNode>
+    | React.ReactPortal
+    | null
+    | undefined;
+}) {
+  return (
+    <div className="card-display">
+      <Row xs={2} md={3} className="g-4">
+        <div className="card-display">
+          <Card className="cards"></Card>
+          <div className="card-display">
+            <Card className="cards">
+              <Card.Img variant="top" src={props.bookImage} />
+              <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text>{props.description}</Card.Text>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroup.Item>Title: {props.title}</ListGroup.Item>
+                <ListGroup.Item>Author: {props.author}</ListGroup.Item>
+                <ListGroup.Item>Genre: {props.genre}</ListGroup.Item>
+              </ListGroup>
+              <Card.Body>
+                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      </Row>
+    </div>
+  );
+}
+
+export default Component;
